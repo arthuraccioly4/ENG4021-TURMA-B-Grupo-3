bcf638d07edb4401c2cf191d88939f6240fea4c1
Julio Diniz <julio.diniz2005@gmail.com>
2026-03-14 19:31:19 +0000

    Criei todos os modulos e o programa principal (calculadora.py), apenas o modulo soma esta completo, os outros estão em branco!

---
+++ Calculadora/calculadora.py
@@ -0,0 +1,28 @@
+import divide
+import multiplica
+import soma
+import subtrai
+
+def main():
+    a = float (input("Digite o primeiro número real: "))
+    b = float (input("Digite o segundo número real: "))
+    operador = input("Qual operção você deseja realizar (+|-|*|/): ")
+
+    if operador == "+":
+        resultado = soma.somaf(a,b)
+    
+    elif operador == "-":
+        resultado = subtrai.subtraif(a,b)
+
+    elif operador == "*":
+        resultado = multiplica.multiplicaf(a,b)
+
+    elif operador == "/":
+        resultado = divide.dividef(a,b)
+    
+    else:
+        resultado = "Número ou operador inválido!"
+
+    print (f"Resultado: {resultado}")
+
+main()
---
+++ Calculadora/soma.py
@@ -0,0 +1,3 @@
+def somaf (x,y):
+    return x + y
+
